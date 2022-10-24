77d26db2ba6ce4ddb3983bdc8ef7459057055068
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2022-10-24 14:50:40 +0000

    chore(deps): upgrade provider project

--- src/group/index.ts
+++ src/group/index.ts
@@ -59,7 +59,7 @@ export class Group extends cdktf.TerraformResource {
       terraformResourceType: 'boundary_group',
       terraformGeneratorMetadata: {
         providerName: 'boundary',
-        providerVersion: '1.1.0',
+        providerVersion: '1.1.2',
         providerVersionConstraint: '~> 1.0'
       },
       provider: config.provider,
