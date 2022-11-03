4b972acd097d6466ea182128c0d89f8dc3f67f7d
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2022-11-03 00:26:34 +0000

    chore(deps): upgrade dependencies
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdktf/cdktf-provider-boundary/actions/runs/3382113509
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*
    
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- scripts/check-for-upgrades.js
+++ scripts/check-for-upgrades.js
@@ -11,7 +11,7 @@ const actions = require("@actions/core");
 
 // set by the projen file that generates this script
 const CONSTRAINT = "~> 1.0";
-const FQ_PROVIDER_NAME = "boundary";
+const FQ_PROVIDER_NAME = "hashicorp/boundary";
 
 (async function main() {
   if (await newerMatchingProviderVersionExists()) {
