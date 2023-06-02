4638f90462a8923185d6bc8c944fb00be7918892
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-06-02 13:46:58 +0000

    chore(deps): Updated

--- src/auth-method-oidc/index.ts
+++ src/auth-method-oidc/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/boundary/1.1.7/docs/resources/auth_method_oidc
 // generated from terraform resource schema
 
