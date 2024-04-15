f483c88a05efc9895823f9aaad8a6a4612f13897
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-04-15 07:08:55 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-boundary-scope/index.ts
+++ src/data-boundary-scope/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/boundary/1.1.14/docs/data-sources/scope
 // generated from terraform resource schema
 
