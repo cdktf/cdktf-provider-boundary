d194f56287733cfe8c0d9ab8c336b646bb41ab36
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-14 05:50:35 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/auth-method-ldap/index.ts
+++ src/auth-method-ldap/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/boundary/1.1.15/docs/resources/auth_method_ldap
 // generated from terraform resource schema
 
